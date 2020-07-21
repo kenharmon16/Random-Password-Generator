06c357e4d695db28ebfc7a158b0c67d153e5d28c
kenharmon16 <kenharmon16@gmail.com>
2020-07-21 17:34:49 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+chars = '!@#$%*1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+number_passwords = int(input("Number of passwords? \n"))
+password_length = int(input("Number of characters? \n"))
+
+for p in range(number_passwords):
+    password = ''
+    for c in range(password_length):
+        password += random.choice(chars)
+    print(password)
